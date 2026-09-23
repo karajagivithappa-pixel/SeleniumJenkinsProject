4b2edf541755653202ba92c9a98811ef87089519
vitha <vitha@192.168.137.36>
2026-09-23 10:37:57 +0530

    commit

--- src/test/java/package1/ContactTest.java
+++ src/test/java/package1/ContactTest.java
@@ -1,5 +1,7 @@
 package package1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class ContactTest {
@@ -11,5 +13,14 @@ public class ContactTest {
 	@Test
 	public void modifyContactTest() {
 		System.out.println("executed modifyContactTest");
+	}
+	
+	@Test
+	public void runContactTest() throws Exception {
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://49.249.29.4:8888/");
+		Thread.sleep(3000);
+		driver.quit();
+		//System.out.println("executed modifyContactTest");
 	}
 }
